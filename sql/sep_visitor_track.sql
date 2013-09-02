73151dfa39c3df4051d0096bd922cda21db07915
yfwang <wayf128@gmail.com>
2013-09-03 02:25:34 +1000

    sql added

---
+++ sql/sep_visitor_track.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `sep` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sep`;
+-- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
+--
+-- Host: localhost    Database: sep
+-- ------------------------------------------------------
+-- Server version	5.5.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `visitor_track`
+--
+
+DROP TABLE IF EXISTS `visitor_track`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `visitor_track` (
+  `group_ID` varchar(50) NOT NULL,
+  `track_ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  KEY `l` (`group_ID`),
+  KEY `ik` (`track_ID`),
+  KEY `s` (`track_ID`),
+  KEY `d` (`group_ID`),
+  CONSTRAINT `d` FOREIGN KEY (`group_ID`) REFERENCES `group_visitor` (`group_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `s` FOREIGN KEY (`track_ID`) REFERENCES `track` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visitor_track`
+--
+
+LOCK TABLES `visitor_track` WRITE;
+/*!40000 ALTER TABLE `visitor_track` DISABLE KEYS */;
+INSERT INTO `visitor_track` VALUES ('9f460aa5-2c8b-4ed7-81ab-ba30cbbbaa75',1,67),('9f460aa5-2c8b-4ed7-81ab-ba30cbbbaa75',3,68),('9f460aa5-2c8b-4ed7-81ab-ba30cbbbaa75',1,69);
+/*!40000 ALTER TABLE `visitor_track` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-03  0:25:05
